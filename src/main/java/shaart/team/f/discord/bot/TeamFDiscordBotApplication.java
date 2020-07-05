25d0e8b7becf9dedc2ef967aaa679daa48e17328
Artur Shalaev <imshaart@gmail.com>
2020-07-05 23:05:11 +0300

    Initial commit

---
+++ src/main/java/shaart/team/f/discord/bot/TeamFDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package shaart.team.f.discord.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFDiscordBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TeamFDiscordBotApplication.class, args);
+  }
+
+}
---
+++ src/test/java/shaart/team/f/discord/bot/TeamFDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package shaart.team.f.discord.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFDiscordBotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
