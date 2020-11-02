4dc5d0c7a7a7ad4ee030d46b4094dcb40d5235cd
Artur Shalaev <imshaart@gmail.com>
2020-11-03 01:51:19 +0300

    Split application into separate modules

---
+++ discord-adapter/src/main/java/com/github/shaart/agile/bot/discord/adapter/DiscordAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.github.shaart.agile.bot.discord.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordAdapterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiscordAdapterApplication.class, args);
+  }
+
+}
---
+++ discord-adapter/src/test/java/com/github/shaart/agile/bot/discord/adapter/DiscordAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shaart.agile.bot.discord.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordAdapterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
